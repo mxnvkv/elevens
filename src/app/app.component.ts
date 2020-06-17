b38d3db51d529ffa56ba74884a3c7891a310a2c6
Maksym Novikov <maksym.novikov12@gmail.com>
2020-06-17 22:49:31 +0300

    Menu closes when we click outside of itself

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
 import { Component, ViewChild, ElementRef, Renderer2 } from '@angular/core';
+import { Observable, fromEvent } from 'rxjs';
+import { map, skip, takeWhile, finalize } from 'rxjs/operators';
 
 @Component({
   selector: 'app-root',
@@ -9,21 +11,25 @@ export class AppComponent {
   @ViewChild('app') app: ElementRef;
   @ViewChild('menu') menu: ElementRef;
 
-  isMenuShowed: boolean = false;
+  clickEvents: Observable<Event> = fromEvent(document, 'click');
 
   constructor(
     private renderer: Renderer2
   ) {}
 
-  showMenu() {
-    if (this.isMenuShowed) {
-      this.renderer.setStyle(this.menu.nativeElement, 'margin-left', '-300px');
-      this.renderer.setStyle(this.app.nativeElement, 'margin-left', '0px');
-      this.isMenuShowed = false;
-    } else {
-      this.renderer.setStyle(this.menu.nativeElement, 'margin-left', '0px');
-      this.renderer.setStyle(this.app.nativeElement, 'margin-left', '300px');
-      this.isMenuShowed = true;
-    }
+  toggleMenu() {
+    this.renderer.setStyle(this.menu.nativeElement, 'margin-left', '0px');
+    this.renderer.setStyle(this.app.nativeElement, 'margin-left', '300px');
+
+    // menu closes when we click outside of itself
+    this.clickEvents.pipe(
+      map((event: Event) => (event.target as Element).className),
+      skip(1),
+      takeWhile(value => value === 'menu'),
+      finalize(() => {
+        this.renderer.setStyle(this.menu.nativeElement, 'margin-left', '-300px');
+        this.renderer.setStyle(this.app.nativeElement, 'margin-left', '0px');
+      })
+    ).subscribe(console.log);
   }
 }

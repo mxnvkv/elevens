494383917aead3b2fbd0c8dc8d2c321dbcf5590f
Maksym Novikov <maksym.novikov12@gmail.com>
2020-06-20 00:20:44 +0300

    Fix unclickable toggle-menu

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -10,6 +10,7 @@ import { map, skip, takeWhile, finalize } from 'rxjs/operators';
 export class AppComponent {
   @ViewChild('app') app: ElementRef;
   @ViewChild('menu') menu: ElementRef;
+  @ViewChild('toggleMenuElement') toggleMenuElement: ElementRef;
 
   clickEvents: Observable<Event> = fromEvent(document, 'click');
 
@@ -21,7 +22,13 @@ export class AppComponent {
     this.renderer.setStyle(this.menu.nativeElement, 'margin-left', '0px');
     this.renderer.setStyle(this.app.nativeElement, 'margin-left', '300px');
 
-    // menu closes when we click outside of itself
+    // removing toggle-menu down, because when
+    // we click on it, menu closes but new observable
+    // starts again and we need to click away to finish it
+    // and be able to open menu
+    this.renderer.setStyle(this.toggleMenuElement.nativeElement, 'z-index', '-1');
+
+    // menu is opened, and closes when we click outside of itself
     this.clickEvents.pipe(
       map((event: Event) => (event.target as Element).className),
       skip(1),
@@ -29,7 +36,10 @@ export class AppComponent {
       finalize(() => {
         this.renderer.setStyle(this.menu.nativeElement, 'margin-left', '-300px');
         this.renderer.setStyle(this.app.nativeElement, 'margin-left', '0px');
+
+        // bringing toggle-menu up
+        this.renderer.setStyle(this.toggleMenuElement.nativeElement, 'z-index', '1');
       })
-    ).subscribe(console.log);
+    ).subscribe((value) => console.log(value), () => {}, () => console.log('completed'));
   }
 }
